fead1cf03c2adbf9f566a887fc199de86c58a57a
Francis_BH <francis.bh.4264@gmail.com>
2022-11-22 10:52:00 -0500

    ejercicio tema 1

---
+++ PrimerProyecto/PrimerProyecto/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PrimerProyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejercicio 1
+            Console.WriteLine("Introduce tu nombre");
+            String nombre = Console.ReadLine();
+            Console.WriteLine("Hola, Bienvenido " + nombre);
+            //Ejercicio 2
+
+            String fecha = DateTime.Now.ToString("hh:mm:ss");
+            Console.WriteLine(fecha);
+
+        }
+    }
+}
