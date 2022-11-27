f4b84410cd2bb5e0f6a972396863e7f95af7484e
Francis_BH <francis.bh.4264@gmail.com>
2022-11-26 20:21:36 -0500

    ejercicio 2 tema 2

--- Ejercicios sesión 2/Ejercicios sesión 2/Program.cs
+++ Ejercicios sesión 2/Ejercicios sesión 2/Program.cs
@@ -6,8 +6,8 @@ namespace Ejercicios_sesión_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            //Ejercicio 1
+          /*  //Ejercicio 1
+            Console.WriteLine("Ejercicio 1: ");
             String Nombre;
             String Apellido;
             int edad;
@@ -28,9 +28,18 @@ namespace Ejercicios_sesión_2
             /*Tipos Usando los tipos de variables más adecuados, describe los objetos siguientes:
             Coche: puertas, ruedas, marca, ITV vigente
             Mesa: peso, largo, material, color
-            */
-            String
-
+            */ /*
+            Console.WriteLine("Ejercicio 2: ");
+            Console.WriteLine("¿Cuantas puertas tiene el coche?");
+            int puerta = int.Parse(Console.ReadLine());
+            Console.WriteLine("¿Cuantas ruedas tiene el coche?");
+            int ruedas = int.Parse(Console.ReadLine());
+            Console.WriteLine("¿Que marca es el coche?");
+            String marca = Console.ReadLine();
+            Console.WriteLine("¿Que color es el coche?");
+            String color = Console.ReadLine();
+            
+            Console.WriteLine("Su coche es: \nMarca: " + marca + "\nTiene: " + puerta + " puertas \nTiene :  " + ruedas + " ruedas \nColor:  " + color);
 
             //Ejercicio 3
             /*
@@ -40,7 +49,33 @@ namespace Ejercicios_sesión_2
             Se cumplen dos conciones a la vez (ambas verdaderas)
             Se cumple una de dos condiciones a la vez (una true y otra false)
             */
+            Console.WriteLine("Ejercicio 3: ");
+            Console.WriteLine("Escriba un número");
+            int Num = int.Parse(Console.ReadLine());
+            if (Num > 18)
+            {
+                Console.WriteLine("El número " + Num + " es mayor a 18");
+            }
+            else if (Num < 18)
+            {
+                Console.WriteLine("El número " + Num + " es menor a 18");
+            }
+            else if (Num == 18)
+            {
+                Console.WriteLine("El número " + Num + " es igual a 18");
+            }
 
-}
-}
+            Console.WriteLine("Escriba el caracter 'a'");
+            char a = char.Parse(Console.ReadLine());
+            if (a == 'a')
+            {
+                Console.WriteLine("El caracter es a");
+            }
+            else
+            {
+                Console.WriteLine("El caracter no es a");
+
+            }
+        }
+    }
 }
